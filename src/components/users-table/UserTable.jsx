179438180dfea970d233b60e1ff7f0b0111ce68c
Amr-Elharerty <amrelharery@gmail.com>
2024-05-11 19:12:29 +0300

    Init part from dahsboard holder-left (user table)

--- src/components/users-table/UserTable.jsx
+++ src/components/users-table/UserTable.jsx
@@ -1,8 +1,26 @@
 import "./UserTable.css";
 
 function UserTable() {
+  let users = [];
   return (
-    <table>User Table</table>
+    <div className="user-tabel">
+      <h2>Users</h2>
+      <div className="responsive-table">
+        <table>
+          <thead>
+            <tr>
+              <th>User ID</th>
+              <th>Date</th>
+              <th>User Name</th>
+              <th>Gender</th>
+              <th>Role</th>
+            </tr>
+          </thead>
+          <tbody>
+          </tbody>
+        </table>
+      </div>
+    </div>
   )
 }
 
