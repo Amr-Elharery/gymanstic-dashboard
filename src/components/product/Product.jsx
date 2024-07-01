d7a201198f4e430e2412e6e0848fdff01f5e2824
Amr-Elharerty <amrelharery@gmail.com>
2024-07-01 19:06:58 +0300

    Finish render products in shop

--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -1,22 +1,41 @@
-import ProductImage from "../../assets/imgs/product-image.png";
 import FavIcon from "../../assets/imgs/fav-icon.png";
 import "./Product.css";
 function Product({product}) {
-  let { title, description, price} = product;
+  let { title, description, quantity, sold, colors, category, price, images} = product;
 
   return (
-    <div className='product bg-white shadow rad-16'>
+    <div className='product flex flex-col bg-white shadow rad-16'>
       <div className="img flex justify-center items-center">
-        <img src={ProductImage} alt="product" />
+        <img src={images[0]} alt="product" />
       </div>
-      <div className="info p-10">
-        <div className="title ">
+      <div className="info p-10 flex flex-col flex-between">
+        <div className="head flex flex-col">
+        <div className="title bold">
           {title}
         </div>
+        <p>
+          Category : {category.name}
+        </p>
         <p className="description ">
           {description}
         </p>
-        <div className="foot flex items-center flex-between ">
+        <hr />
+        <p>
+          Colors : <span>{colors.map((color, i) => (i === colors.length-1 ? color: color+" - "))}</span>
+        </p>
+        <hr />
+        <p>
+          Quantity : {quantity}
+        </p>
+        <hr />
+        <p>
+          Sold: {sold}
+        </p>
+        <hr />
+
+        </div>
+
+        <div className="foot flex items-center flex-between border-black rad-16 p-10">
         <p className="price">
           ${price}
         </p>
@@ -24,6 +43,7 @@ function Product({product}) {
         <div className="icon">
           <img src={FavIcon} alt="favourite" />
         </div>
+
         </div>
       </div>
     </div>
--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -1,62 +1,89 @@
 import "./Products.css";
 import Product from "../product/Product";
+import { useEffect, useState } from "react";
 function Products() {
-    let products = [
-        {
-            id:1,
-            title: "AAI",
-            description:"TAC/IO LZT Voult Board",
-            price: 1255.00
-        },
-        {
-            id:2,
-            title: "AAI",
-            description:"TAC/IO LZT Voult Board",
-            price: 1255.00
-        },
-        {
-            id:3,
-            title: "AAI",
-            description:"TAC/IO LZT Voult Board",
-            price: 1255.00
-        },
-        {
-            id:4,
-            title: "AAI",
-            description:"TAC/IO LZT Voult Board",
-            price: 1255.00
-        },
-        {
-            id:5,
-            title: "AAI",
-            description:"TAC/IO LZT Voult Board",
-            price: 1255.00
-        },
-        {
-            id:6,
-            title: "AAI",
-            description:"TAC/IO LZT Voult Board",
-            price: 1255.00
-        },
-        {
-            id:7,
-            title: "AAI",
-            description:"TAC/IO LZT Voult Board",
-            price: 1255.00
-        },
-        {
-            id:8,
-            title: "AAI",
-            description:"TAC/IO LZT Voult Board",
-            price: 1255.00
-        },
-        {
-            id:9,
-            title: "AAI",
-            description:"TAC/IO LZT Voult Board",
-            price: 1255.00
-        },
-    ]
+    const [products, setProducts] = useState([]);
+
+    const [id, setId] = useState("");
+    const [token, setToken] = useState("");
+    useEffect(() => {
+      let authData = localStorage.getItem("authorization") 
+                  || sessionStorage.getItem("authorization");
+  
+      if (authData) {
+        authData = JSON.parse(authData);
+        setId(authData.id);
+        setToken(authData.token);
+      }
+    }, []);
+
+    useEffect(() => {
+        fetch("https://gymnastic-beta.vercel.app/api/v1/products", {
+            headers: {
+                "Authorization": `Bearer ${token}`
+            }
+        })
+       .then(res => res.json())
+       .then(resObj => {
+        setProducts(resObj.data);
+    })
+    },[id,  token])
+    // let products = [
+    //     {
+    //         id:1,
+    //         title: "AAI",
+    //         description:"TAC/IO LZT Voult Board",
+    //         price: 1255.00
+    //     },
+    //     {
+    //         id:2,
+    //         title: "AAI",
+    //         description:"TAC/IO LZT Voult Board",
+    //         price: 1255.00
+    //     },
+    //     {
+    //         id:3,
+    //         title: "AAI",
+    //         description:"TAC/IO LZT Voult Board",
+    //         price: 1255.00
+    //     },
+    //     {
+    //         id:4,
+    //         title: "AAI",
+    //         description:"TAC/IO LZT Voult Board",
+    //         price: 1255.00
+    //     },
+    //     {
+    //         id:5,
+    //         title: "AAI",
+    //         description:"TAC/IO LZT Voult Board",
+    //         price: 1255.00
+    //     },
+    //     {
+    //         id:6,
+    //         title: "AAI",
+    //         description:"TAC/IO LZT Voult Board",
+    //         price: 1255.00
+    //     },
+    //     {
+    //         id:7,
+    //         title: "AAI",
+    //         description:"TAC/IO LZT Voult Board",
+    //         price: 1255.00
+    //     },
+    //     {
+    //         id:8,
+    //         title: "AAI",
+    //         description:"TAC/IO LZT Voult Board",
+    //         price: 1255.00
+    //     },
+    //     {
+    //         id:9,
+    //         title: "AAI",
+    //         description:"TAC/IO LZT Voult Board",
+    //         price: 1255.00
+    //     },
+    // ]
   return (
     <div className="products">
         {products.map((p)=>(<Product key={p.id} product={p} />))}
